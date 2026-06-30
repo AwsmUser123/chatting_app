a8fce69e8fa3fdb35a1b3e3dec4ee17a35601d93
AwsmUser123 <johoop2002@gmail.com>
2026-06-30 22:20:22 +0200

    Added some more functionality for the future.

--- client.c
+++ client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,21 +10,49 @@
 #define SERV_PORT 6879
 #define QUEUE_LEN 5
 
+void handle_error(char *);
+
 int main() {
     struct sockaddr_in addr;
     int server;
     char buf;
 
-    buf = 0;
+    buf = 0x00;
     server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+        handle_error("Socket creation failed.\n");
+    }
     addr.sin_family = AF_INET;
     addr.sin_port = SERV_PORT;
-    inet_aton(SERV_ADDR, &(addr.sin_addr));
-
-    connect(server, (struct sockaddr *)(&addr), sizeof(addr));
-    while (1) {
-        read(0, &buf, 1);
-        write(server, &buf, 1);
+    if (inet_aton(SERV_ADDR, &(addr.sin_addr)) == 0) {
+        handle_error("Invalid server address.\n");
     }
+    if (connect(server, (struct sockaddr *)(&addr), sizeof(addr)) == -1) {
+        handle_error("Failed to connect to server.\n");
+    }
+
+    printf("\e[1;1H\e[2J");
+    printf("Welcome to the simple chatting application!\n");
+    printf("Running the initial test...\n");
+
+    buf = 0x01;
+    if (write(server, &buf, 1) < 1) {
+        handle_error("Failed to send data to the server.\n");
+    }
+    if (read(server, &buf, 1) < 1) {
+        handle_error("Failed to receive data from the server.\n");
+    }
+    if (buf == 0x02) {
+        printf("OK!\n");
+    }
+    else {
+        handle_error("Initial test failed. Exiting...\n");
+    }
+
     return 0;
-} 
+}
+
+void handle_error(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
--- server.c
+++ server.c
@@ -1,37 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 
 #define SERV_ADDR "192.168.2.186"
 #define SERV_PORT 6879
 #define QUEUE_LEN 5
 
+void handle_error(char *);
+void *handle_client(void *);
+
 int main() {
     struct sockaddr_in addr;
-    int fd, client;
-    int running;
+    int server, client;
     int i;
     char buf;
     struct sockaddr_in clients[QUEUE_LEN];
+    socklen_t client_len[QUEUE_LEN];
+    pthread_t threads[QUEUE_LEN];
 
-    buf = 0;
-    running = 1;
+    buf = 0x00;
     i = 0;
-    fd = socket(AF_INET, SOCK_STREAM, 0);
+    server = socket(AF_INET, SOCK_STREAM, 0);
+    if (server == -1) {
+        handle_error("Socket creation failed.\n");
+    }
+
     addr.sin_family = AF_INET;
     addr.sin_port = SERV_PORT;
-    inet_aton(SERV_ADDR, &(addr.sin_addr));
-    bind(fd, (struct sockaddr *)(&addr), sizeof(addr));
-    listen(fd, QUEUE_LEN);
+    if (inet_aton(SERV_ADDR, &(addr.sin_addr)) == 0) {
+        handle_error("Invalid server address.\n");
+    }
+    if (bind(server, (struct sockaddr *)(&addr), sizeof(addr)) == -1) {
+        handle_error("Failed to bind the address to the socket.\n");
+    }
+    if (listen(server, QUEUE_LEN) == -1) {
+        handle_error("Failed to mark the socket as listening.\n");
+    }
     
-
-    do {
-        client = accept(fd, (struct sockaddr *)(clients + i), sizeof(clients[i]));
+    while (i < QUEUE_LEN) {
+        client_len[i] = sizeof(struct sockaddr_in);
+        client = accept(server, (struct sockaddr *)(&clients[i]), &client_len[i]);
+        if (client == -1) {
+            handle_error("Failed to accept a client connection.\n");
+        }
+        if (pthread_create(&threads[i], NULL, handle_client, (void *)&client) != 0) {
+            handle_error("Failed to create a thread for the client connection.\n");
+        }
         i++;
-        spawn_thread(client);
-    } while (running);
+    }
+
+    for (i = 0; i < QUEUE_LEN; i++) {
+        if (pthread_join(threads[i], NULL) != 0) {
+            handle_error("Failed to join a thread.\n");
+        }
+    }
+
+    printf("Server finished working.\n");
     return 0;
 } 
+
+void *handle_client(void *arg) {
+    int client = *((int *)arg);
+    char buf;
+    buf = 0x00;
+
+    while (1) {
+        if (read(client, &buf, 1) < 1) {
+            handle_error("Failed to receive data from client.\n");
+        }
+        if (buf == 0x01) {
+            buf = 0x02;
+            if (write(client, &buf, 1) < 1) {
+                handle_error("Failed to send data to client.\n");
+            }
+            pthread_exit(NULL);
+        }
+    }
+}
+
+void handle_error(char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
