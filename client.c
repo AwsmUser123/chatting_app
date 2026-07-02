1582fa9058c250a419a2c1184598557c3856fb92
AwsmUser123 <johoop2002@gmail.com>
2026-07-02 16:27:30 +0200

    Added login functionality to the app

--- client.c
+++ client.c
@@ -1,7 +1,9 @@
+#include <ctype.h>
 #include <curses.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -12,8 +14,11 @@
 #define SERV_PORT 6879
 #define QUEUE_LEN 5
 #define BUF_SIZE 1024
+#define NAME_LEN 128
+#define PASS_LEN 128
 
 void handle_error(char *);
+int credentials_valid(char *);
 
 int main() {
     struct sockaddr_in addr;
@@ -56,20 +61,57 @@ int main() {
         handle_error("Failed to read a response from user.\n");
 
     switch (response) {
-        case 1:
+        case 1: {
             // do account creation stuff
             break;
-        case 2:
+        }
+        case 2: {
             // do login stuff
+            int data_len;
+            char account_name[NAME_LEN];
+            char account_password[PASS_LEN];
+
+            printf("Please enter your account name: ");
+            if (scanf("%s", account_name) < 1)
+                handle_error("Failed to read account name.\n");
+            if (!credentials_valid(account_name))
+                handle_error("Invalid account name.\n");
+
+            printf("Please enter your password: ");
+            if (scanf("%s", account_password) < 1)
+                handle_error("Failed to read password.\n");
+            if (!credentials_valid(account_password))
+                handle_error("Invalid password.\n");
+            buf[0] = 0x05;
+            data_len = strlen(account_name) + strlen(account_password) + 1;
+            strcpy(buf+1, account_name);
+            buf[strlen(account_name)+1] = ':';
+            strcpy(buf+strlen(account_name)+2, account_password);
+            if (write(server, buf, 1) < 1)
+                handle_error("Failed to send data to the server.\n");
+            if (write(server, &data_len, 4) < 4)
+                handle_error("Failed to send data to the server.\n");
+            if (write(server, buf+1, data_len) < data_len)
+                handle_error("Failed to send data to the server.\n");
+            if (read(server, buf, 1) < 1)
+                handle_error("Failed to receive data from the server.\n");
+            if (buf[0] == 0x06)
+                printf("Logged in!\n");
+            else
+                handle_error("Login failed.\n");
             break;
-        case 3:
+        }
+        case 3: {
+            // quit
             buf[0] = 0x20;
             if (write(server, buf, 1) < 1)
                 handle_error("Failed to send data to the server.\n");
             printf("Thank you for using the application.\n");
             break;
-        default:
+        }
+        default: {
             handle_error("Unknown code.\n");
+        }
     }
 
     return 0;
@@ -81,4 +123,13 @@ void handle_error(char *msg) {
     else
         fprintf(stderr, "%s", msg);
     exit(EXIT_FAILURE);
+}
+
+int credentials_valid(char *str) {
+    while (*str) {
+        if (!isalnum(*str))
+            return 0;
+        str++;
+    }
+    return 1;
 }
--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -12,6 +13,8 @@
 #define SERV_PORT 6879
 #define QUEUE_LEN 5
 #define BUF_SIZE 1024
+#define NAME_LEN 128
+#define PASS_LEN 128
 
 void handle_error(char *);
 void *handle_client(void *);
@@ -26,40 +29,33 @@ int main() {
 
     i = 0;
     server = socket(AF_INET, SOCK_STREAM, 0);
-    if (server == -1) {
+    if (server == -1)
         handle_error("Socket creation failed.\n");
-    }
 
     addr.sin_family = AF_INET;
     addr.sin_port = SERV_PORT;
-    if (inet_aton(SERV_ADDR, &(addr.sin_addr)) == 0) {
+    if (inet_aton(SERV_ADDR, &(addr.sin_addr)) == 0)
         handle_error("Invalid server address.\n");
-    }
-    if (bind(server, (struct sockaddr *)(&addr), sizeof(addr)) == -1) {
+    if (bind(server, (struct sockaddr *)(&addr), sizeof(addr)) == -1)
         handle_error("Failed to bind the address to the socket.\n");
-    }
-    if (listen(server, QUEUE_LEN) == -1) {
+    if (listen(server, QUEUE_LEN) == -1)
         handle_error("Failed to mark the socket as listening.\n");
-    }
     
     printf("Successfully started the server.\n");
     while (i < QUEUE_LEN) {
         client_len[i] = sizeof(struct sockaddr_in);
         client = accept(server, (struct sockaddr *)(&clients[i]), &client_len[i]);
-        if (client == -1) {
+        if (client == -1)
             handle_error("Failed to accept a client connection.\n");
-        }
-        if (pthread_create(&threads[i], NULL, handle_client, (void *)&client) != 0) {
+        if (pthread_create(&threads[i], NULL, handle_client, (void *)&client) != 0)
             handle_error("Failed to create a thread for the client connection.\n");
-        }
         printf("Successfully accepted an incoming connection.\n");
         i++;
     }
 
     for (i = 0; i < QUEUE_LEN; i++) {
-        if (pthread_join(threads[i], NULL) != 0) {
+        if (pthread_join(threads[i], NULL) != 0)
             handle_error("Failed to join a thread.\n");
-        }
     }
 
     printf("Server finished working.\n");
@@ -76,27 +72,85 @@ void handle_error(char *msg) {
 
 void *handle_client(void *arg) {
     int client = *((int *)arg);
+    int index;
+    int client_id = -1;
     char buf[BUF_SIZE];
 
     while (1) {
-        if (read(client, buf, 1) < 1) {
+        if (read(client, buf, 1) < 1)
             handle_error("Failed to receive data from client.\n");
-        }
         switch (buf[0]) {
-            case 0x00:
+            case 0x00: {
                 //Error
                 pthread_exit(NULL);
-            case 0x01:
+            }
+            case 0x01: {
                 buf[0] = 0x02;
                 if (write(client, buf, 1) < 1) {
                     handle_error("Failed to send data to client.\n");
                 }
                 break;
-            case 0x20:
+            }
+            case 0x05: {
+                int data_len;
+                FILE *accounts;
+                char *res;
+                char account_name[NAME_LEN];
+                char account_password[PASS_LEN];
+
+                if (client_id != -1)
+                    handle_error("The client is already logged in.\n");
+
+                if (read(client, &data_len, 4) < 4)
+                    handle_error("Failed to read data size.\n");
+                if (data_len >= BUF_SIZE)
+                    handle_error("Data provided by client is too big.\n");
+                if (read(client, buf, data_len) < data_len)
+                    handle_error("Failed to receive login data from client.\n");
+                buf[data_len] = '\0';
+
+                res = strtok(buf, ":\n");
+                if (res == NULL)
+                    handle_error("Failed to read account name from client.\n");
+                strcpy(account_name, res);
+                res = strtok(NULL, ":\n");
+                if (res == NULL)
+                    handle_error("Failed to read account password from client.\n");
+                strcpy(account_password, res);
+
+                if ((accounts = fopen("accounts.txt", "r")) == NULL)
+                    handle_error("Failed to open accounts file.\n");
+                index = 0;
+                while (fgets(buf, BUF_SIZE, accounts) != NULL) {
+                    res = strtok(buf, ":\n");
+                    if (res == NULL)
+                        handle_error("Failed to read account name from account file.\n");
+                    if (!strcmp(res, account_name)) {
+                        res = strtok(NULL, ":\n");
+                        if (res == NULL)
+                            handle_error("Failed to read account password from account file.\n");
+                        if (!strcmp(res, account_password)) {
+                            client_id = index;
+                            buf[0] = 0x06;
+                            if (write(client, buf, 1) < 1)
+                                handle_error("Failed to send login confirmation to client.\n");
+                            break;
+                        }
+                        else
+                            handle_error("Wrong password.\n");
+                    }
+                }
+                if (client_id == -1)
+                    handle_error("Account with that username does not exist.\n");
+                break;
+            }
+            case 0x20: {
                 //Regular quit
                 pthread_exit(NULL);
-            default:
+            }
+            default: {
                 handle_error("Server received unknown operation code.\n");
+            }
         }
     }
 }
