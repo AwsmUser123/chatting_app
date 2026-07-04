897f4cc2e5dc520be71576aa830d6fc9f4ec0202
AwsmUser123 <johoop2002@gmail.com>
2026-07-04 14:22:11 +0200

    Separated the functionality into more functions and added some future plans to the TODO.txt file.

--- client.c
+++ client.c
@@ -17,7 +17,9 @@
 #define NAME_LEN 128
 #define PASS_LEN 128
 
+void print_message(int);
 void handle_error(char *);
+void handle_response(int);
 int credentials_valid(char *);
 
 int main() {
@@ -46,107 +48,140 @@ int main() {
     buf[0] = 0x01;
     if (write(server, buf, 1) < 1)
         handle_error("Failed to send data to the server.\n");
-    if (read(server, buf, 1) < 1)
-        handle_error("Failed to receive data from the server.\n");
-    if (buf[0] == 0x02)
-        printf("OK!\n");
-    else
-        handle_error("Initial test failed. Exiting...\n");
+    handle_response(server);
 
-    printf("What would you like to do?\n");
-    printf("1) - Create an account.\n");
-    printf("2) - Log into an account.\n");
-    printf("3) - Quit.\n");
-    if (scanf("%d", &response) < 1)
-        handle_error("Failed to read a response from user.\n");
+    int running = 1;
+    do {
+        printf("What would you like to do?\n");
+        printf("1) - Create an account.\n");
+        printf("2) - Log into an account.\n");
+        printf("3) - Create a new chat.\n");
+        printf("4) - Join an existing chat.\n");
+        printf("5) - List all existing chats.\n");
+        printf("6) - Quit.\n");
+        if (scanf("%d", &response) < 1)
+            handle_error("Failed to read a response from user.\n");
 
-    switch (response) {
-        case 1: {
-            // do account creation stuff
-            int data_len;
-            char account_name[NAME_LEN];
-            char account_password[PASS_LEN];
+        switch (response) {
+            case 1: {
+                // do account creation stuff
+                int data_len;
+                char account_name[NAME_LEN];
+                char account_password[PASS_LEN];
 
-            printf("Please enter your new account name: ");
-            if (scanf("%s", account_name) < 1)
-                handle_error("Failed to read account name.\n");
-            if (!credentials_valid(account_name))
-                handle_error("Invalid account name.\n");
+                printf("Please enter your new account name: ");
+                if (scanf("%s", account_name) < 1)
+                    handle_error("Failed to read account name.\n");
+                if (!credentials_valid(account_name))
+                    handle_error("Invalid account name.\n");
 
-            printf("Please enter your new password: ");
-            if (scanf("%s", account_password) < 1)
-                handle_error("Failed to read password.\n");
-            if (!credentials_valid(account_password))
-                handle_error("Invalid password.\n");
-            buf[0] = 0x03;
-            data_len = strlen(account_name) + strlen(account_password) + 1;
-            strcpy(buf+1, account_name);
-            buf[strlen(account_name)+1] = ':';
-            strcpy(buf+strlen(account_name)+2, account_password);
-            if (write(server, buf, 1) < 1)
-                handle_error("Failed to send data to the server.\n");
-            if (write(server, &data_len, 4) < 4)
-                handle_error("Failed to send data to the server.\n");
-            if (write(server, buf+1, data_len) < data_len)
-                handle_error("Failed to send data to the server.\n");
-            if (read(server, buf, 1) < 1)
-                handle_error("Failed to receive data from the server.\n");
-            if (buf[0] == 0x04)
-                printf("Your account has been successfully created!\n");
-            else
-                handle_error("Account creation failed failed.\n");
-            break;
+                printf("Please enter your new password: ");
+                if (scanf("%s", account_password) < 1)
+                    handle_error("Failed to read password.\n");
+                if (!credentials_valid(account_password))
+                    handle_error("Invalid password.\n");
+                buf[0] = 0x03;
+                data_len = strlen(account_name) + strlen(account_password) + 1;
+                strcpy(buf+1, account_name);
+                buf[strlen(account_name)+1] = ':';
+                strcpy(buf+strlen(account_name)+2, account_password);
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                if (write(server, &data_len, 4) < 4)
+                    handle_error("Failed to send data to the server.\n");
+                if (write(server, buf+1, data_len) < data_len)
+                    handle_error("Failed to send data to the server.\n");
+                handle_response(server);
+                break;
+            }
+            case 2: {
+                // do login stuff
+                int data_len;
+                char account_name[NAME_LEN];
+                char account_password[PASS_LEN];
+
+                printf("Please enter your account name: ");
+                if (scanf("%s", account_name) < 1)
+                    handle_error("Failed to read account name.\n");
+                if (!credentials_valid(account_name))
+                    handle_error("Invalid account name.\n");
+
+                printf("Please enter your password: ");
+                if (scanf("%s", account_password) < 1)
+                    handle_error("Failed to read password.\n");
+                if (!credentials_valid(account_password))
+                    handle_error("Invalid password.\n");
+                buf[0] = 0x05;
+                data_len = strlen(account_name) + strlen(account_password) + 1;
+                strcpy(buf+1, account_name);
+                buf[strlen(account_name)+1] = ':';
+                strcpy(buf+strlen(account_name)+2, account_password);
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                if (write(server, &data_len, 4) < 4)
+                    handle_error("Failed to send data to the server.\n");
+                if (write(server, buf+1, data_len) < data_len)
+                    handle_error("Failed to send data to the server.\n");
+                handle_response(server);
+                break;
+            }
+            case 3: {
+                // do chat creation stuff
+                buf[0] = 0x07;
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                print_message(server);
+                handle_response(server);
+                break;
+            }
+            case 4: {
+                // do chat joining stuff
+                buf[0] = 0x09;
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                long chat_id;
+                printf("Please enter the ID of the chat you would like to join: ");
+                if (scanf("%ld", &chat_id) < 1)
+                    handle_error("Failed to read the ID of the chat from the user.\n");
+                if (write(server, &chat_id, 4) < 4)
+                    handle_error("Failed to send data to the server.\n");
+                handle_response(server);
+                break;
+            }
+            case 5: {
+                // list all available chats
+                buf[0] = 0x10;
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                print_message(server);
+                break;
+            }
+            case 6: {
+                // quit
+                buf[0] = 0x20;
+                if (write(server, buf, 1) < 1)
+                    handle_error("Failed to send data to the server.\n");
+                printf("Thank you for using the application.\n");
+                running = 0;
+                break;
+            }
+            default: {
+                handle_error("Unknown code.\n");
+            }
         }
-        case 2: {
-            // do login stuff
-            int data_len;
-            char account_name[NAME_LEN];
-            char account_password[PASS_LEN];
-
-            printf("Please enter your account name: ");
-            if (scanf("%s", account_name) < 1)
-                handle_error("Failed to read account name.\n");
-            if (!credentials_valid(account_name))
-                handle_error("Invalid account name.\n");
-
-            printf("Please enter your password: ");
-            if (scanf("%s", account_password) < 1)
-                handle_error("Failed to read password.\n");
-            if (!credentials_valid(account_password))
-                handle_error("Invalid password.\n");
-            buf[0] = 0x05;
-            data_len = strlen(account_name) + strlen(account_password) + 1;
-            strcpy(buf+1, account_name);
-            buf[strlen(account_name)+1] = ':';
-            strcpy(buf+strlen(account_name)+2, account_password);
-            if (write(server, buf, 1) < 1)
-                handle_error("Failed to send data to the server.\n");
-            if (write(server, &data_len, 4) < 4)
-                handle_error("Failed to send data to the server.\n");
-            if (write(server, buf+1, data_len) < data_len)
-                handle_error("Failed to send data to the server.\n");
-            if (read(server, buf, 1) < 1)
-                handle_error("Failed to receive data from the server.\n");
-            if (buf[0] == 0x06)
-                printf("Logged in!\n");
-            else
-                handle_error("Login failed.\n");
-            break;
-        }
-        case 3: {
-            // quit
-            buf[0] = 0x20;
-            if (write(server, buf, 1) < 1)
-                handle_error("Failed to send data to the server.\n");
-            printf("Thank you for using the application.\n");
-            break;
-        }
-        default: {
-            handle_error("Unknown code.\n");
-        }
-    }
+    } while (running);
 
     return 0;
+}
+
+void print_message(int server_fd) {
+    char buf[BUF_SIZE];
+    int data_len;
+    if (read(server_fd, &data_len, 4) < 4)
+        handle_error("Failed to read the data from the server.\n");
+    if (read(server_fd, buf, data_len) < data_len)
+        handle_error("Failed to read the data from the server.\n");
+    puts(buf);
 }
 
 void handle_error(char *msg) {
@@ -155,6 +190,25 @@ void handle_error(char *msg) {
     else
         fprintf(stderr, "%s", msg);
     exit(EXIT_FAILURE);
+}
+
+void handle_response(int server_fd) {
+    char buf[BUF_SIZE];
+    if (read(server_fd, buf, 1) < 1)
+        handle_error("Failed to read data from server.\n");
+    if (buf[0] == 0x00) {
+        int data_len;
+        if (read(server_fd, &data_len, 4) < 4)
+            handle_error("Failed to read data from server.\n");
+        if (read(server_fd, buf, data_len) < data_len)
+            handle_error("Failed to read data from server.\n");
+        printf("ERROR: %s", buf);
+    }
+    else if (buf[0] == 0x02) {
+        printf("OK!\n");
+    }
+    else
+        handle_error("Unknown response code from server.\n");
 }
 
 int credentials_valid(char *str) {
--- server.c
+++ server.c
@@ -15,11 +15,37 @@
 #define BUF_SIZE 1024
 #define NAME_LEN 128
 #define PASS_LEN 128
+#define MAX_MEMBERS 10
 
+struct text_list {
+    char *text;
+    time_t *date;
+    struct text_list *next;
+};
+
+struct chat_item {
+    long chat_id;
+    struct text_list *contents;
+    int curr_member;
+    int members[MAX_MEMBERS];
+    struct chat_item *next;
+};
+
+struct arg_data {
+    int client_fd;
+    struct chat_item **head;
+};
+
+void send_message(int, char *);
+void send_confirmation(int);
+void send_error(int, char *);
 void handle_error(char *);
 void *handle_client(void *);
 int login_user(int);
 void register_user(int);
+void create_chat(int, int, struct chat_item **);
+void join_chat(int, int, struct chat_item **);
+void get_username(int, char *);
 
 int main() {
     struct sockaddr_in addr;
@@ -28,8 +54,10 @@ int main() {
     struct sockaddr_in clients[QUEUE_LEN];
     socklen_t client_len[QUEUE_LEN];
     pthread_t threads[QUEUE_LEN];
+    struct chat_item *first_chat;
 
     i = 0;
+    first_chat = NULL;
     server = socket(AF_INET, SOCK_STREAM, 0);
     if (server == -1)
         handle_error("Socket creation failed.\n");
@@ -49,7 +77,10 @@ int main() {
         client = accept(server, (struct sockaddr *)(&clients[i]), &client_len[i]);
         if (client == -1)
             handle_error("Failed to accept a client connection.\n");
-        if (pthread_create(&threads[i], NULL, handle_client, (void *)&client) != 0)
+        struct arg_data args;
+        args.client_fd = client;
+        args.head = &first_chat;
+        if (pthread_create(&threads[i], NULL, handle_client, (void *)&args) != 0)
             handle_error("Failed to create a thread for the client connection.\n");
         printf("Successfully accepted an incoming connection.\n");
         i++;
@@ -72,8 +103,39 @@ void handle_error(char *msg) {
     exit(EXIT_FAILURE);
 }
 
+void send_confirmation(int client_fd) {
+    char code = 0x02;
+    if (write(client_fd, &code, 1) < 1)
+        handle_error("Failed to send confirmation to client.\n");
+} 
+
+void send_message(int client_fd, char *msg) {
+    char buf[BUF_SIZE];
+    int data_len;
+    data_len = strlen(msg)+1;
+    memcpy(buf, &data_len, 4);
+    strcpy(buf+4, msg);
+    if (write(client_fd, buf, data_len+4) < data_len+4)
+        handle_error("Failed to send the data to the client.\n");
+}
+
+void send_error(int client_fd, char *msg) {
+    char buf[BUF_SIZE];
+    int data_len;
+    buf[0] = 0x00;
+    if (errno != 0)
+        sprintf(buf+5, "%s\nError:%s", msg, strerror(errno));
+    else
+        sprintf(buf+5, "%s", msg);
+    data_len = strlen(buf+5)+1;
+    memcpy(buf+1, &data_len, 4);
+    if (write(client_fd, buf, data_len+5) < data_len+5)
+        handle_error("Failed to send the error to the client.\n");
+}
+
 void *handle_client(void *arg) {
-    int client = *((int *)arg);
+    int client = ((struct arg_data *)arg)->client_fd;
+    struct chat_item **head = ((struct arg_data *)arg)->head;
     int client_id = -1;
     char buf[BUF_SIZE];
 
@@ -81,15 +143,8 @@ void *handle_client(void *arg) {
         if (read(client, buf, 1) < 1)
             handle_error("Failed to receive data from client.\n");
         switch (buf[0]) {
-            case 0x00: {
-                //Error
-                pthread_exit(NULL);
-            }
             case 0x01: {
-                buf[0] = 0x02;
-                if (write(client, buf, 1) < 1) {
-                    handle_error("Failed to send data to client.\n");
-                }
+                send_confirmation(client);
                 break;
             }
             case 0x03: {
@@ -98,19 +153,44 @@ void *handle_client(void *arg) {
             }
             case 0x05: {
                 if (client_id != -1)
-                    handle_error("The client is already logged in.\n");
-                client_id = login_user(client);
+                    send_error(client, "You are already logged in.\n");
+                else
+                    client_id = login_user(client);
+                break;
+            }
+            case 0x07: {
+                create_chat(client, client_id, head);
+                break;
+            }
+            case 0x09: {
+                join_chat(client, client_id, head);
+                break;
+            }
+            case 0x10: {
+                char tmp[BUF_SIZE];
+                char username[NAME_LEN];
+                struct chat_item *curr = *head;
+                strcpy(buf, "");
+                while (curr != NULL) {
+                    sprintf(tmp, "CHAT ID: %ld\n", curr->chat_id);
+                    strcat(buf, tmp);
+                    strcat(buf, "Members:\n");
+                    for (int j = 0; j < curr->curr_member; j++) {
+                        get_username(curr->members[j], username);
+                        sprintf(tmp, "\t%s\n", username);
+                        strcat(buf, tmp);
+                    }
+                    curr = curr->next;
+                }
+                send_message(client, buf);
                 break;
             }
             case 0x20: {
-                //Regular quit
                 pthread_exit(NULL);
             }
             default: {
                 handle_error("Server received unknown operation code.\n");
             }
-            //USED TEMPORARILY TO AVOID ERRORS. REMOVE AS SOON AS POSSIBLE
-            pthread_exit(NULL);
         }
     }
 }
@@ -154,57 +234,148 @@ int login_user(int client_fd) {
                 handle_error("Failed to read account password from account file.\n");
             if (!strcmp(res, account_password)) {
                 index = i;
-                buf[0] = 0x06;
-                if (write(client_fd, buf, 1) < 1)
-                    handle_error("Failed to send login confirmation to client.\n");
                 break;
             }
-            else
-                handle_error("Wrong password.\n");
+            else {
+                send_error(client_fd, "Wrong password.\n");
+                return -1;
+            }
         }
     }
     if (index == -1)
         handle_error("Account with that username does not exist.\n");
+    else
+        send_confirmation(client_fd);
     return index;
 }
 
 void register_user(int client_fd) {
-        int data_len;
-        FILE *accounts;
-        char *res;
-        char buf[BUF_SIZE];
-        char account_name[NAME_LEN];
-        char account_password[PASS_LEN];
+    int data_len;
+    FILE *accounts;
+    char *res;
+    char buf[BUF_SIZE];
+    char account_name[NAME_LEN];
+    char account_password[PASS_LEN];
 
-        if (read(client_fd, &data_len, 4) < 4)
-            handle_error("Failed to read data size.\n");
-        if (data_len >= BUF_SIZE)
-            handle_error("Data provided by client is too big.\n");
-        if (read(client_fd, buf, data_len) < data_len)
-            handle_error("Failed to receive login data from client.\n");
-        buf[data_len] = '\0';
+    if (read(client_fd, &data_len, 4) < 4)
+        handle_error("Failed to read data size.\n");
+    if (data_len >= BUF_SIZE)
+        handle_error("Data provided by client is too big.\n");
+    if (read(client_fd, buf, data_len) < data_len)
+        handle_error("Failed to receive login data from client.\n");
+    buf[data_len] = '\0';
 
+    res = strtok(buf, ":\n");
+    if (res == NULL)
+        handle_error("Failed to read account name from client.\n");
+    strcpy(account_name, res);
+    res = strtok(NULL, ":\n");
+    if (res == NULL)
+        handle_error("Failed to read account password from client.\n");
+    strcpy(account_password, res);
+
+    if ((accounts = fopen("accounts.txt", "r+")) == NULL)
+        handle_error("Failed to open accounts file.\n");
+    while (fgets(buf, BUF_SIZE, accounts) != NULL) {
         res = strtok(buf, ":\n");
         if (res == NULL)
-            handle_error("Failed to read account name from client.\n");
-        strcpy(account_name, res);
-        res = strtok(NULL, ":\n");
-        if (res == NULL)
-            handle_error("Failed to read account password from client.\n");
-        strcpy(account_password, res);
-
-        if ((accounts = fopen("accounts.txt", "r+")) == NULL)
-            handle_error("Failed to open accounts file.\n");
-        while (fgets(buf, BUF_SIZE, accounts) != NULL) {
-            res = strtok(buf, ":\n");
-            if (res == NULL)
-                handle_error("Failed to read account name from account file.\n");
-            if (!strcmp(res, account_name))
-                handle_error("Account with that username already exists.\n");
+            handle_error("Failed to read account name from account file.\n");
+        if (!strcmp(res, account_name)) {
+            send_error(client_fd, "Account with that username already exists.\n");
+            return;
         }
-        if (fprintf(accounts, "%s:%s\n", account_name, account_password) < 0)
-            handle_error("Failed to write new data to the accounts file.\n");
-        buf[0] = 0x04;
-        if (write(client_fd, buf, 1) < 1)
-            handle_error("Failed to send confirmation to client.\n");
+    }
+    if (fprintf(accounts, "%s:%s\n", account_name, account_password) < 0)
+        handle_error("Failed to write new data to the accounts file.\n");
+    send_confirmation(client_fd);
+}
+
+void create_chat(int client_fd, int client_id, struct chat_item **head) {
+    struct chat_item *curr;
+    char buf[BUF_SIZE];
+    if (client_id == -1) {
+        send_error(client_fd, "You are not logged in.\n");
+        return;
+    }
+    if (*head == NULL) {
+        *head = malloc(sizeof(struct chat_item));
+        if (*head == NULL)
+            handle_error("Failed to allocate memory.\n");
+        curr = *head;
+    }
+    else {
+        curr = *head;
+        while (curr->next != NULL)
+            curr = curr->next;
+        curr->next = malloc(sizeof(struct chat_item));
+        if (curr->next == NULL)
+            handle_error("Failed to allocate memory.\n");
+        curr = curr->next;
+    }
+    curr->chat_id = random();
+    curr->contents = NULL;
+    curr->curr_member = 1;
+    curr->members[0] = client_id;
+    curr->next = NULL;
+    sprintf(buf, "CHAT ID: %ld\n", curr->chat_id);
+    send_message(client_fd, buf);
+    send_confirmation(client_fd);
+}
+
+void join_chat(int client_fd, int client_id, struct chat_item **head) {
+    if (client_id == -1) {
+        send_error(client_fd, "You are not logged in.\n");
+        return;
+    }
+    long chat_id;
+    int found = 0;
+    if (read(client_fd, &chat_id, 4) < 4)
+        handle_error("Failed to read data from client.\n");
+    if (*head != NULL) {
+        struct chat_item *curr = *head;
+        while (curr->next != NULL) {
+            if (curr->chat_id == chat_id) {
+                found = 1;
+                if (curr->curr_member < MAX_MEMBERS) {
+                    for (int i = 0; i < curr->curr_member; i++) {
+                        if (curr->members[i] == client_id) {
+                            send_error(client_fd, "You are already a member of that chat.\n");
+                            return;
+                        }
+                    }
+                    curr->members[curr->curr_member] = client_id;
+                    curr->curr_member++;
+                    break;
+                }
+                else {
+                    send_error(client_fd, "The chat is full.\n");
+                    return;
+                }
+            }
+            curr = curr->next;
+        }
+    }
+    if (found == 1)
+        send_confirmation(client_fd);
+    else 
+        send_error(client_fd, "Could not find a chat with that ID.\n");
+}
+
+void get_username(int client_id, char *result) {
+    char buf[BUF_SIZE];
+    char *res;
+    FILE *accounts;
+    int i = 0;
+    if ((accounts = fopen("accounts.txt", "r")) == NULL)
+        handle_error("Failed to open accounts file.\n");
+    while (fgets(buf, BUF_SIZE, accounts) != NULL) {
+        res = strtok(buf, ":\n");
+        if (res == NULL)
+            handle_error("Failed to read account name from account file.\n");
+        if (i == client_id) {
+            strcpy(result, res);
+            return;
+        }
+        i++;
+    }
 }
